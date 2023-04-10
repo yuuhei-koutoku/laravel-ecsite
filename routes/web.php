aa435f21d13625c4d2a35cbb715d6b6f210ee2a1
yuuhei-koutoku <yhkt.0627@gmail.com>
2023-04-10 15:39:32 +0900

    service container test

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ComponentTestController;
+use App\Http\Controllers\LifeCycleTestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,6 +26,7 @@ Route::get('/dashboard', function () {
 
 Route::get('component-test1', [ComponentTestController::class, 'showComponent1']);
 Route::get('component-test2', [ComponentTestController::class, 'showComponent2']);
+Route::get('servicecontainertest', [LifeCycleTestController::class, 'showServiceContainerTest']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
