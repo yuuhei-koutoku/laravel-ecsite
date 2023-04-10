20ca01604ec490ecaf975f689717d0fb8d1defa5
yuuhei-koutoku <yhkt.0627@gmail.com>
2023-04-10 10:06:52 +0900

    component variable

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -7,7 +7,9 @@ use Illuminate\Http\Request;
 class ComponentTestController extends Controller
 {
     public function showComponent1(){
-        return view('tests.component-test1');
+        $message = 'メッセージ123';
+        return view('tests.component-test1',
+        compact('message'));
     }
 
     public function showComponent2(){
