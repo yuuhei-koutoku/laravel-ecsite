1a7263d4a94519801609cacc0f0b8b7b49ce7d3b
yuuhei-koutoku <yhkt.0627@gmail.com>
2023-04-11 14:34:18 +0900

    multi login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,13 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+            @if (auth('admin')->user())
+                @include('layouts.admin-navigation')
+            @elseif (auth('owners')->user())
+                @include('layouts.owner-navigation')
+            @elseif (auth('users')->user())
+                @include('layouts.user-navigation')
+            @endif
 
             <!-- Page Heading -->
             @if (isset($header))
