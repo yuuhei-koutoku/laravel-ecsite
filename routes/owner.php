6ee4a618a97cdf7187881053c46f2fc7ee572b68
yuuhei-koutoku <yhkt.0627@gmail.com>
2023-05-09 14:49:55 +0900

    middleware, abort(404)

--- routes/owner.php
+++ routes/owner.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Owner\Auth\NewPasswordController;
 use App\Http\Controllers\Owner\Auth\PasswordController;
 use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
+use App\Http\Controllers\Owner\ShopController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 
 /*
@@ -25,6 +26,13 @@ use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 
 Route::get('/', function () {
     return view('owner.welcome');
+});
+
+Route::prefix('shops')->
+    middleware('auth:owners')->group(function(){
+        Route::get('index', [ShopController::class, 'index'])->name('shops.index');
+        Route::get('edit/{shop}', [ShopController::class, 'edit'])->name('shops.edit');
+        Route::post('update/{shop}', [ShopController::class, 'update'])->name('shops.update');
 });
 
 Route::get('/dashboard', function () {
