d866f09985dad89402cb3321e80f96b98cf031e5
yuuhei-koutoku <yhkt.0627@gmail.com>
2023-05-10 13:49:10 +0900

    form request

--- resources/views/owner/shops/edit.blade.php
+++ resources/views/owner/shops/edit.blade.php
@@ -16,7 +16,7 @@
                                 <div class="relative">
                                     <label for="image" class="leading-7 text-sm text-gray-600">画像</label>
                                     <input type="file" id="image" name="image" accept="image/png,image/jpeg,image/jpg" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                    {{-- <x-input-error :messages="$errors->get('name')" class="mt-2" /> --}}
+                                    <x-input-error :messages="$errors->get('image')" class="mt-2" />
                                 </div>
                                 <div class="p-2 w-full flex justify-around mt-4">
                                     <button type="button" onclick="location.href='{{ route('owner.shops.index') }}'" class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
