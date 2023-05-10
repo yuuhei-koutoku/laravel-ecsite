7c2616080353b1d01f233ae067d3bae2ac197556
yuuhei-koutoku <yhkt.0627@gmail.com>
2023-05-10 09:43:06 +0900

    display index

--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -1,19 +1,37 @@
 <x-app-layout>
-  <x-slot name="header">
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-          {{ __('Dashboard') }}
-      </h2>
-  </x-slot>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 
-  <div class="py-12">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-              <div class="p-6 text-gray-900">
-                  @foreach ($shops as $shop)
-                    {{ $shop->name }}
-                  @endforeach
-              </div>
-          </div>
-      </div>
-  </div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    @foreach ($shops as $shop)
+                        <div class="w-1/2 p-4">
+                            <a href="{{ route('owner.shops.edit', ['shop' => $shop->id]) }}">
+                                <div class="border rounded-md p-4">
+                                    @if($shop->is_selling)
+                                        <span class ="border p-2 rounded-md bg-blue-400 text-white">販売中</span>
+                                    @else
+                                    <span class ="border p-2 rounded-md bg-red-400 text-white">停止中</span>
+                                    @endif
+                                </div>
+                                <div class="text-xl">{{ $shop->name }}</div>
+                                <div>
+                                    @if(empty($shop->filename))
+                                        <img src="{{ asset('images/no_image.jpg') }}">
+                                    @else
+                                        <img src="{{ asset('storage/shops/' . $shop->filename) }}">
+                                    @endif
+                                </div>
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
