0a0b3a874ed5583ede6d37dba26991d166d23f4f
yuuhei-koutoku <yhkt.0627@gmail.com>
2023-05-10 17:26:14 +0900

    disconnect to service

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use InterventionImage;
 use App\Http\Requests\UploadImageRequest;
+use App\Services\ImageService;
 
 class ShopController extends Controller
 {
@@ -52,14 +53,15 @@ class ShopController extends Controller
     {
         $imageFile = $request->image;
         if (!is_null($imageFile) && $imageFile->isValid()) {
-            // Storage::putFile('public/shops', $imageFile); // リサイズなしの場合
-            $fileName = uniqid(rand() . '_');
-            $extension = $imageFile->extension();
-            $fileNameToStore = $fileName . '.' . $extension;
-            $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
-            // dd($imageFile, $resizedImage);
+            $fileNameToStore = ImageService::upload($imageFile, 'shops');
+            // // Storage::putFile('public/shops', $imageFile); // リサイズなしの場合
+            // $fileName = uniqid(rand() . '_');
+            // $extension = $imageFile->extension();
+            // $fileNameToStore = $fileName . '.' . $extension;
+            // $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+            // // dd($imageFile, $resizedImage);
 
-            Storage::put('public/shops/' . $fileNameToStore, $resizedImage);
+            // Storage::put('public/shops/' . $fileNameToStore, $resizedImage);
         }
 
         return to_route('owner.shops.index');
