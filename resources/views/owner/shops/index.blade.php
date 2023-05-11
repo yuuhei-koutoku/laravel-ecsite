14019c70048f9e62a5fc9a69184fbff76707b632
yuuhei-koutoku <yhkt.0627@gmail.com>
2023-05-11 11:07:16 +0900

    image index

--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -21,7 +21,7 @@
                                     @endif
                                 </div>
                                 <div class="text-xl">{{ $shop->name }}</div>
-                                <x-shop-thumbnail :filename="$shop->filename" />
+                                <x-thumbnail :filename="$shop->filename" type="shops" />
                             </a>
                         </div>
                     @endforeach
