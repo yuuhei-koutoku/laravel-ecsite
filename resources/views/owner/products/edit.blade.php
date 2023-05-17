da5be276e7441f11e8d51c7f2c7334c7b93c25fe
yuuhei-koutoku <yhkt.0627@gmail.com>
2023-05-17 14:29:52 +0900

    product update

--- resources/views/owner/products/edit.blade.php
+++ resources/views/owner/products/edit.blade.php
@@ -9,6 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
+                    <x-flash-message status="session('status')" />
                     <form method="post" action="{{ route('owner.products.update', ['product' => $product->id]) }}" >
                         @csrf
                         @method('put')
