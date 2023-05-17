680438600b63e0d3ce831210a30347e51b982b38
yuuhei-koutoku <yhkt.0627@gmail.com>
2023-05-17 15:39:18 +0900

    product destory

--- resources/views/owner/products/edit.blade.php
+++ resources/views/owner/products/edit.blade.php
@@ -119,6 +119,13 @@
                             </div>
                         </div>
                     </form>
+                    <form id="delete_{{ $product->id }}" method="post" action="{{ route('owner.products.destroy', ['product' => $product->id]) }}">
+                        @csrf
+                        @method('delete')
+                        <div class="p-2 w-full flex justify-around mt-32">
+                            <a href="#" data-id="{{ $product->id }}" onclick="deletePost(this)" class="text-white bg-red-400 border-0 py-2 px-4 focus:outline-none hover:bg-red-500 rounded ">削除する</a>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
@@ -139,5 +146,12 @@
                 MicroModal.close(modal);
             }, )
         })
+
+        function deletePost(e) {
+            'use strict';
+            if (confirm('本当に削除してもよろしいですか？')) {
+                document.getElementById('delete_' + e.dataset.id).submit();
+            }
+        }
     </script>
 </x-app-layout>
