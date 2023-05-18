552dbef71dd9cebb7fb69304e53daead33e9aa27
yuuhei-koutoku <yhkt.0627@gmail.com>
2023-05-18 14:54:04 +0900

    product factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
+use App\Models\Stock;
 
 class DatabaseSeeder extends Seeder
 {
@@ -30,5 +32,7 @@ class DatabaseSeeder extends Seeder
             // StockSeeder::class,
             UserSeeder::class,
         ]);
+        Product::factory(100)->create();
+        Stock::factory(100)->create();
     }
 }
