88fb3321ca1556cdd76fb277d5d5258f7ef10abd
yuuhei-koutoku <yhkt.0627@gmail.com>
2023-05-23 09:40:13 +0900

    cart delete

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -44,4 +44,13 @@ class CartController extends Controller
 
         return to_route('user.cart.index');
     }
+
+    public function delete($id)
+    {
+        Cart::where('product_id', $id)
+        ->where('user_id', Auth::id())
+        ->delete();
+
+        return to_route('user.cart.index');
+    }
 }
