e6656168e2d52f1b602baf3e52b3e1c37986aa74
yuuhei-koutoku <yhkt.0627@gmail.com>
2024-08-16 11:26:10 +0900

    guest login

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Http\Controllers\User\GuestLoginController;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -42,6 +43,8 @@ class ProfileController extends Controller
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
+        if (GuestLoginController::isGuestUser()) return redirect()->intended('/profile');
+
         $request->user()->fill($request->validated());
 
         if ($request->user()->isDirty('email')) {
@@ -58,6 +61,8 @@ class ProfileController extends Controller
      */
     public function destroy(Request $request): RedirectResponse
     {
+        if (GuestLoginController::isGuestUser()) return redirect()->intended('/profile');
+
         $request->validateWithBag('userDeletion', [
             'password' => ['required', 'current-password'],
         ]);
--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -15,6 +15,11 @@ use Illuminate\Support\Facades\DB;
 
 class CartController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:users');
+    }
+
     public function index()
     {
         $user = User::findOrFail(Auth::id());
