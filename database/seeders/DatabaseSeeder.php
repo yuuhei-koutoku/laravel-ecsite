1e684e57ce92e16c95f1c51636066c77e0cd8027
yuuhei-koutoku <yhkt.0627@gmail.com>
2024-09-03 23:45:30 +0900

    fix image, product and stock seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,23 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
         $this->call([
             AdminSeeder::class,
             OwnerSeeder::class,
             ShopSeeder::class,
             ImageSeeder::class,
             CategorySeeder::class,
-            // ProductSeeder::class,
-            // StockSeeder::class,
+            ProductSeeder::class,
+            StockSeeder::class,
             UserSeeder::class,
         ]);
-        Product::factory(1000)->create();
-        Stock::factory(1000)->create();
     }
 }
