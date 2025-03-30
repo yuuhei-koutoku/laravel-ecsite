81dd194ec28ebd2e913f57e6513bf06bed8df257
yuuhei-koutoku <yhkt.0627@gmail.com>
2025-03-30 14:51:19 +0900

    owner managed index test

---
+++ tests/Feature/Http/Controllers/Admin/OwnersControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Admin;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Owner;
+
+class OwnersControllerTest extends TestCase
+{
+    public function test_オーナー一覧を表示()
+    {
+        $this->adminLogin();
+
+        Owner::factory()->createMany([
+            ['name' => '鈴木'],
+            ['name' => '伊藤'],
+        ]);
+
+        $response = $this->get('admin/owners');
+
+        $response
+            ->assertOk()
+            ->assertSee(['鈴木', '伊藤']);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,20 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Admin;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use RefreshDatabase;
+
+    protected function adminLogin($user = null)
+    {
+        $user ??= Admin::factory()->create();
+
+        $this->actingAs($user, 'admin');
+
+        return $user;
+    }
 }
